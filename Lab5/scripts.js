8254f74855cd2aa4f329ef9205d0814e6004b2ff
Nikita Suslov <nikita.suslov@ingka.ikea.com>
2020-12-14 00:40:36 +0300

    lab 5

---
+++ Lab5/scripts.js
@@ -0,0 +1,13 @@
+function OpenNewWindow1(){
+	window.open("PageForOpen1.html", "OpenWindow", "width = '100px', height = '200'");
+};
+function OpenNewWindow2(){
+	window.open("PageForOpen2.html", "OpenWindow", "width = '100px', height = '200'");
+};
+function PromptInsrt(){
+	temp =prompt("Please enter something...");
+	textbox1.value = temp;
+};
+function CloseWindow(){
+	window.close();
+};
